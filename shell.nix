239a6e8efdb6ccb189a805beef30148eb8445d6c
marcusnewton <marcus.newton@hotmail.com>
2021-09-14 18:13:53 +1000

    add npm publish scripts

--- shell.nix
+++ shell.nix
@@ -1,10 +1,28 @@
 let
  pkgs = import <nixpkgs> {};
+
+ prepack = pkgs.writeShellScriptBin "prepack" ''
+ npm run compile
+
+ mkdir artifacts
+ cp build/contracts/*.json artifacts
+ '';
+
+ pack = pkgs.writeShellScriptBin "pack" ''
+ npm pack
+ '';
+
+ publish = pkgs.writeShellScriptBin "publish" ''
+ npm publish ./balancer-core-0.0.7.tgz --access public
+ '';
 in
 pkgs.stdenv.mkDerivation {
  name = "shell";
  buildInputs = [
   pkgs.nodejs-14_x
+  prepack
+  pack
+  publish
  ];
 
  shellHook = ''
