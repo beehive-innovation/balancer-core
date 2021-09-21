af254170a20ac713d8e759b3f24967cd126f5287
marcusnewton <marcus.newton@hotmail.com>
2021-09-21 17:17:59 +1000

    add publish npm workflow

--- shell.nix
+++ shell.nix
@@ -1,40 +1,31 @@
 let
- pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { };
 
- prepack = pkgs.writeShellScriptBin "prepack" ''
- npm run compile
+  prepack = pkgs.writeShellScriptBin "prepack" ''
+    npm run compile
 
- rm -rf artifacts
- mkdir -p artifacts
- cp build/contracts/*.json artifacts
- '';
+    rm -rf artifacts
+    mkdir -p artifacts
+    cp build/contracts/*.json artifacts
+  '';
 
- prepublish = pkgs.writeShellScriptBin "prepublish" ''
-  npm config set sign-git-tag true
-  npm version patch
- '';
-
- publish = pkgs.writeShellScriptBin "publish" ''
-  echo "//registry.npmjs.org/:_authToken=''${NPM_TOKEN}" > .npmrc
-
-  PACKAGE_NAME=$(node -p "require('./package.json').name")
-  PACKAGE_VERSION=$(node -p "require('./package.json').version")
-  npm pack
-  npm publish ./$PACKAGE_NAME-$PACKAGE_VERSION.tgz --access public
- '';
+  prepublish = pkgs.writeShellScriptBin "prepublish" ''
+    npm config set sign-git-tag true
+    npm version patch
+  '';
 in
 pkgs.stdenv.mkDerivation {
- name = "shell";
- buildInputs = [
-  pkgs.nodejs-14_x
-  prepack
-  prepublish
-  publish
- ];
+  name = "shell";
+  buildInputs = [
+    pkgs.nodejs-14_x
+    pkgs.nixpkgs-fmt
+    prepack
+    prepublish
+  ];
 
- shellHook = ''
-  export PATH=$( npm bin ):$PATH
-  # keep it fresh
-  npm install
- '';
+  shellHook = ''
+    export PATH=$( npm bin ):$PATH
+    # keep it fresh
+    npm install
+  '';
 }
